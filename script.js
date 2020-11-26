f0c01c0676c7d65ad068390d0ea168c1d3def056
Max1 <sfmg88@gmail.com>
2020-11-26 18:07:14 +0200

    test

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+// const obj ={
+//   name: "John",
+//   age: 25,
+//   isMarried: false
+// };
+
+// console.log(obj.name, obj.age);
+
+// let arr = ['plumb.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+// console.log(arr[2]);
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your lastname?', '');
+// answers[2] = prompt('How old?', '');
+// console.log(answers);
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert (`Привет, ${user}`)
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
