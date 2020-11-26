43128d2fec3c103cdf5aab88b38255364ef02866
Max <sfmg88@gmail.com>
2020-11-27 00:46:35 +0200

    practice2

--- script.js
+++ script.js
@@ -43,3 +43,60 @@
 // console.log(++incr);
 // console.log(--decr);
 
+// if (4 == 9) {
+//   console.log('ok!');
+// } else {
+//   console.log('error');
+// }
+
+
+
+// if (num < 49) {
+//   console.log('error');
+// } else if (num > 100) {
+//   console.log('Много');
+// } else {
+//   console.log('ok!');
+// }
+
+// (num === 50) ? console.log('ok!') : console.log('error');
+
+// const num = 50;
+// switch (num) {
+//     case 49: 
+//       console.log('Neverno');
+//       break;
+//     case 100:
+//       console.log('Neverno');
+//       break;
+//     case 50:
+//       console.log('ok');
+//       break;
+//     default:
+//       console.log('не сейчас');
+// }
+
+
+
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// let num = 50;
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     // break;
+//     continue;
+//   }
+//   console.log(i);
+  
+// }
